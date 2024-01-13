29b040018a66f770d436019098b772f1a048404f
Oaltunji Abdulkadir <tunjikadri@gmail.com>
2024-01-13 23:07:41 +0100

    Router added

--- app/app.js
+++ app/app.js
@@ -8,11 +8,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended:true}));
 //use middleware to handle cors
 app.use(cors());
-
+//health point or actuator
 app.get('/', (req,res,next) => {
     res.status(200).json({message: "Service is Running"});
 });
 //router Links
+
+//bad url or error  handle method with middleware
+app.use((req, res, next)=>{
+    const error = new Error('Not Found');
+    error.status= 400;
+    next(error);
+});
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500).json({
+        error:{
+        message: error.message,
+        status: error.status
+    }})
+});
 
 module.exports = app;
 
