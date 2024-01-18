bc3052a49a62fbb30c5fd7bbdd9e2dc963918568
Olatunji Abdulkadir <tunjikadri@gmail.com>
2024-01-18 12:55:40 +0100

    test suite added

---
+++ src/test/blog.test.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose")
+const request = require("supertest")
+const app = require("../../index")
+require("dotenv").config();
+const { reqaddBlog, requpdateBlog } = require("../utils/data/blog.test.data.js");
+
+const token = "{YOUR TOKEN>}" 
+let blogId = 
+//Create blog Test 
+describe("POST /api/blog/create", () => {
+    test("should create a blog", async () => {
+        return request(app)
+            .post("/api/blog/create")
+            .set('Authorization',  `Bearer ${token}`)
+            .send(reqaddBlog)
+            .expect(201)
+            .then(({ body })=>{
+                blogId = body.data.blogId
+            })
+
+    });
+});
+
+
+describe("GET /api/blog/get", () => {
+    test("should return all blogs", async () => {
+        return request(app)
+            .get("/api/blog/get")
+            .expect('Content-Type', /json/)
+            .expect(200)
+    });
+});
+
+describe("GET /api/blog/show/:id", () => {
+    test('should return all blogs', async () => {
+        return request(app)
+            .get(`/api/blog/show/${blogId}`)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    });
+})
+
+describe("PUT /api/blog/update/:id", () => {
+    test("should update a blog", async () => {
+        return request(app)
+            .put(`/api/blog/update/${blogId}`)
+            .set('Authorization',  `Bearer ${token}`)
+            .send(requpdateBlog)
+            .expect(201)
+    });
+});
+
+describe("Checking authorization middleware", () => {
+    test("should create a blog", async () => {
+        return request(app)
+            .post("/api/blog/create")
+            .send(reqaddblog)
+            .expect(401)
+    });
+});
+
+describe("DELETE /api/blog/delete/:id", () => {
+    test("should create a blog", async () => {
+        return request(app)
+            .delete(`/api/blog/delete/${blogId}`)
+            .set('Authorization', `Bearer ${token}`)
+            .expect(410)
+    });
+});
---
+++ src/utils/data/blog.test.data.js
@@ -0,0 +1,15 @@
+const reqaddBlog = {
+    title:"Blog test", 
+    content:"This is blogtest", 
+   
+ }
+
+const requpdateBlog = {
+    title:"Blog update", 
+    content:"This is Blog test for update", 
+    }
+
+module.exports = {
+  reqaddBlog, 
+  requpdateBlog
+}
